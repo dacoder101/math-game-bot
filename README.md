115a2265d9bb920af6f3516505ef43bd7b8a9691
dacoder101 <bobbye23899@gmail.com>
2024-02-06 21:25:23 -0800

    Basic discord.py configuration

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ A discord bot for a simple math game, written with Discord.py
 
 Install all dependencies using `poetry install` after cloning the repository. You will need to configure your IDE with the virtual environment it generates, or it may not detect the installed dependencies.
 
-The `TOKEN` environment variable must be set with your bot's token, which can be created and found on the [Discord Developer Portal](https://discord.com/developers/).
+The `MATH_BOT_TOKEN` environment variable must be set with your bot's token, which can be created and found on the [Discord Developer Portal](https://discord.com/developers/).
 
 Thank you for contributing!
---
+++ math_game_bot/bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from discord.ext import commands
+
+
+class MathGameBot(commands.Bot):
+    def __init__(self, token):
+        super().__init__(command_prefix="!", intents=discord.Intents.default())
+        self.token = token
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user.name}")
+
+    @commands.command()
+    async def ping(self, ctx):
+        '''Measures the bot's latency.'''
+        latency = round(self.latency * 1000)
+        await ctx.send(f"Pong! Latency: {latency}ms")
+
+    def run_bot(self):
+        '''Start the bot.'''
+        self.run(self.token)
+
+
+TOKEN = os.environ.get("MATH_BOT_TOKEN")
+bot = MathGameBot(TOKEN)
+bot.run_bot()
