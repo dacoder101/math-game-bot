92ab8ac8acb579adce4f264a7c4a26469789cb64
dacoder <bobbye23899@hotmail.com>
2024-02-07 19:53:32 +0000

    New command, remove test directory

--- math_game_bot/bot.py
+++ math_game_bot/bot.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 
 class MathGameBot(commands.Bot):
     """Math game bot for Discord."""
+
     def __init__(self, token):
         super().__init__(command_prefix="!", intents=discord.Intents.default())
         self.token = token
@@ -20,6 +21,12 @@ class MathGameBot(commands.Bot):
         """Measures the bot's latency."""
         latency = round(self.latency * 1000)
         await ctx.send(f"Pong! Latency: {latency}ms")
+
+    @commands.command()
+    async def servers(self, ctx):
+        """Prints the number of servers the bot is in."""
+        servers = len(self.guilds)
+        await ctx.send(f"The bot is in {servers} servers.")
 
     def run_bot(self):
         """Start the bot."""
