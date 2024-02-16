29df58ea0e450f224261e13ef6de6f7fa916c2e1
dacoder <bobbye23899@gmail.com>
2024-02-16 20:00:41 +0000

    `generate_operator`, `is_digit` methods

--- math_game_bot/lexer.py
+++ math_game_bot/lexer.py
@@ -1,25 +1,10 @@
 """Lexer for the math game bot."""
 
-# from .token_type import TokenType
+from .token_type import TokenType
+from .token import Token
 
-from enum import Enum
 
-
-class TokenType(Enum):
-    """Mathematical token types."""
-
-    NUM = 1
-    ADD = 2
-    SUB = 3
-    MUL = 4
-    DIV = 5
-    MOD = 7
-    EXP = 8
-    FACTORIAL = 9
-    SQRT = 10
-    LOG = 11
-    LPAREN = 12
-    RPAREN = 13
+WHITESPACE = " \n\t"
 
 
 class Lexer:
@@ -46,6 +31,21 @@ class Lexer:
 
         self.keywords = {"sqrt": TokenType.EXP, "log": TokenType.LOG}
 
+    def is_digit(self, char):
+        """Check if a character is a digit, or decimal point."""
+        return char.isdigit() or char == "."
+
+    def generate_tokens(self):
+        """Generate tokens from the equation."""
+
+        while self.current_char is not None:
+
+            if self.current_char in WHITESPACE:
+                self.advance()
+
+            elif self.current_char.isdigit() or self.current_char == ".":
+                yield self.generate_number()
+
     def advance(self):
         """Advance the iterator and return the next character."""
 
@@ -54,9 +54,26 @@ class Lexer:
         except StopIteration:
             self.current_char = None
 
-    def generate_tokens(self):
-        """Generate tokens from the equation."""
+    def generate_number(self):
+        """Generate a number token from the equation."""
 
-        while self.current_char is not None:
-            
-            pass
+        number = self.current_char
+        self.advance()
+
+        while self.current_char is not None and self.is_digit(self.current_char):
+            number += self.current_char
+            self.advance()
+
+            if number.count(".") > 1:
+                break
+
+        number += self.current_char
+        self.advance()
+
+        if number.startswith("."):
+            number = "0" + number
+
+        if number.endswith("."):
+            number += "0"
+
+        return Token(TokenType.NUM, float(number))
