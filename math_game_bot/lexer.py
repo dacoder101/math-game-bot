2a6b306f94db14c5bc96ac1745cf1e5c1465aa88
dacoder <bobbye23899@gmail.com>
2024-02-16 19:26:42 +0000

    Placeholder `while` loop

--- math_game_bot/lexer.py
+++ math_game_bot/lexer.py
@@ -1,6 +1,25 @@
 """Lexer for the math game bot."""
 
-from .token_type import TokenType
+# from .token_type import TokenType
+
+from enum import Enum
+
+
+class TokenType(Enum):
+    """Mathematical token types."""
+
+    NUM = 1
+    ADD = 2
+    SUB = 3
+    MUL = 4
+    DIV = 5
+    MOD = 7
+    EXP = 8
+    FACTORIAL = 9
+    SQRT = 10
+    LOG = 11
+    LPAREN = 12
+    RPAREN = 13
 
 
 class Lexer:
@@ -8,6 +27,7 @@ class Lexer:
 
     def __init__(self, equation):
         self.equation = iter(equation.lower())
+        self.advance()
 
         self.operations = {
             "+": TokenType.ADD,
@@ -30,11 +50,13 @@ class Lexer:
         """Advance the iterator and return the next character."""
 
         try:
-            return next(self.equation)
+            self.current_char = next(self.equation)
         except StopIteration:
-            return None
+            self.current_char = None
 
     def generate_tokens(self):
         """Generate tokens from the equation."""
 
-        pass
+        while self.current_char is not None:
+            
+            pass
