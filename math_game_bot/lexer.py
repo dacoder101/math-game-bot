93d3feb061ffdc8ece27715baeb776e128c09b6c
dacoder <bobbye23899@gmail.com>
2024-02-16 21:34:00 +0000

    `is_operator` and `generate_operator` methods

--- math_game_bot/lexer.py
+++ math_game_bot/lexer.py
@@ -5,6 +5,25 @@ from .token import Token
 
 
 WHITESPACE = " \n\t"
+OPERATIONS = {
+    "+": TokenType.ADD,
+    "-": TokenType.SUB,
+    "*": TokenType.MUL,
+    "x": TokenType.MUL,
+    "/": TokenType.DIV,
+    "%": TokenType.MOD,
+    "^": TokenType.EXP,
+    "!": TokenType.FACTORIAL,
+    "(": TokenType.LPAREN,
+    ")": TokenType.RPAREN,
+    "[": TokenType.LPAREN,
+    "]": TokenType.RPAREN,
+}
+
+KEYWORDS = {
+    "sqrt": TokenType.EXP,
+    "log": TokenType.LOG,
+}
 
 
 class Lexer:
@@ -14,26 +33,13 @@ class Lexer:
         self.equation = iter(equation.lower())
         self.advance()
 
-        self.operations = {
-            "+": TokenType.ADD,
-            "-": TokenType.SUB,
-            "*": TokenType.MUL,
-            "x": TokenType.MUL,
-            "/": TokenType.DIV,
-            "%": TokenType.MOD,
-            "^": TokenType.EXP,
-            "!": TokenType.FACTORIAL,
-            "(": TokenType.LPAREN,
-            ")": TokenType.RPAREN,
-            "[": TokenType.LPAREN,
-            "]": TokenType.RPAREN,
-        }
-
-        self.keywords = {"sqrt": TokenType.EXP, "log": TokenType.LOG}
-
     def is_digit(self, char):
         """Check if a character is a digit, or decimal point."""
         return char.isdigit() or char == "."
+
+    def is_operator(self, char):
+        """Check if a character is an operator."""
+        return char in OPERATIONS
 
     def generate_tokens(self):
         """Generate tokens from the equation."""
@@ -45,6 +51,9 @@ class Lexer:
 
             elif self.is_digit(self.current_char):
                 yield self.generate_number()
+
+            elif self.is_operator(self.current_char):
+                yield self.generate_operator()
 
     def advance(self):
         """Advance the iterator and return the next character."""
@@ -77,3 +86,11 @@ class Lexer:
             number += "0"
 
         return Token(TokenType.NUM, float(number))
+
+    def generate_operator(self):
+        """Generate an operation token from the equation."""
+
+        operation = self.current_char
+        self.advance()
+
+        return Token(OPERATIONS[operation], operation)
