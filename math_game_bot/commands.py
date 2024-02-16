2ce4d73638c077eee832f979f6cda015b0f23e7b
dacoder <bobbye23899@gmail.com>
2024-02-16 07:50:43 +0000

    `commands.py` formatting, `Embed` and `Interaction` imports, placeholder `equation_list` command method

--- math_game_bot/commands.py
+++ math_game_bot/commands.py
@@ -1,5 +1,6 @@
 """Command functionality for the bot."""
 
+from discord import Embed, Interaction
 import discord
 
 
@@ -7,14 +8,16 @@ def setup_commands(bot):
     """Command functionality class for the bot."""
 
     @bot.tree.command(name="ping", description="Display the ping in milliseconds.")
-    async def ping(interaction: discord.Interaction):
+    async def ping(interaction: Interaction):
         """Display the ping in milliseconds."""
 
         latency = bot.latency * 1000
         await interaction.response.send_message(f"Pong! {latency:.2f}ms")
 
-    @bot.tree.command(name="server-count", description="Display the number of servers the bot is in.")
-    async def server_count(interaction: discord.Interaction):
+    @bot.tree.command(
+        name="server-count", description="Display the number of servers the bot is in."
+    )
+    async def server_count(interaction: Interaction):
         """Display the number of servers the bot is in."""
 
         message = (
@@ -24,13 +27,25 @@ def setup_commands(bot):
         )
 
         await interaction.response.send_message(message)
-    
+
     @bot.tree.command(name="new-game", description="Start a new math game.")
-    async def new_game(interaction: discord.Interaction):
+    async def new_game(interaction: Interaction):
         """Start a new math game."""
+
         pass
 
-    @bot.tree.command(name="submit-equation", description="Submit an equation for the math game.")
-    async def submit_equation(interaction: discord.Interaction):
+    @bot.tree.command(
+        name="equations", description="List the equations for the math game."
+    )
+    async def equation_list(interaction: Interaction):
+        """List the equations for submitted into the current math game."""
+
+        pass
+
+    @bot.tree.command(
+        name="submit-equation", description="Submit an equation for the math game."
+    )
+    async def submit_equation(interaction: Interaction):
         """Submit an equation for the math game."""
+
         pass
