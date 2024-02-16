798dc916b00c05fcdcd98e215627777238e73d49
dacoder <bobbye23899@gmail.com>
2024-02-16 17:09:37 +0000

    Token class placeholder

---
+++ math_game_bot/token.py
@@ -0,0 +1,11 @@
+"""Token functionality for operators and keyword tokens utilized."""
+
+from dataclasses import dataclass
+from .token_type import TokenType
+
+
+@dataclass
+class Token:
+    """Token functionality class for operators and keyword tokens utilized."""
+
+    pass
