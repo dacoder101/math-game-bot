d8c9cd8628f1efdd8bef0bf27a5124ef360c6cc4
dacoder <bobbye23899@gmail.com>
2024-02-19 08:13:09 +0000

    Translate integer to dictionary

--- math_game_bot/bot/validate.py
+++ math_game_bot/bot/validate.py
@@ -3,17 +3,24 @@
 from dataclasses import dataclass
 from ..exceptions import InvalidCharactersError
 
-
+@dataclass
 class ValidateIntegers:
     """Methods to validate an expression."""
 
     ints: str
+
+    def int_capability(self, x):
+        try:
+            int(x) 
+            return True
+        except ValueError:
+            return False
     
     def get_numerical_form(self, x):
         """Return `x` as integer, or raise an error if impossible."""
 
         try:
-            return int(x)
+          return int(x)
         except ValueError as e:
             raise InvalidCharactersError("Int dictionary is invalid") from e
     
@@ -26,3 +33,15 @@ class ValidateIntegers:
         """Verify values in the dictionary."""
 
         return x > 0
+    
+    def validate(self):
+        """Validate; raise an exception or return a verified dictionary."""
+
+        ints = self.ints
+
+        if self.int_capability(ints):
+
+            ints = self.get_numerical_form(ints)
+
+            if self.verify_key(self.get_numerical_form(ints)):
+                return {ints: 1}
