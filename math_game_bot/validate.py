4c182cb301408a5f5180dbf942926839c3191db5
dacoder <bobbye23899@gmail.com>
2024-02-20 07:07:40 +0000

    `validate.py` method changes and additions

--- math_game_bot/validate.py
+++ math_game_bot/validate.py
@@ -10,7 +10,15 @@ class ValidateIntegers:
 
     ints: str
 
+    def __post_init__(self):
+        """Remove trailing comma from string."""
+
+        self.ints = self.ints.strip()
+
+        self.ints = self.remove_trailing_comma()
+
     def int_capability(self, x):
+        """Return True if `x` is an integer, False otherwise."""
         try:
             int(x)
             return True
@@ -35,6 +43,34 @@ class ValidateIntegers:
 
         return x > 0
 
+    def remove_trailing_comma(self):
+        """Remove trailing comma from string."""
+
+        if self.ints[-1] == ",":
+            return self.ints[:-1]
+
+        return self.ints
+
+    def ints_to_list(self):
+        """Convert a string to the list type."""
+
+        pass
+
+    def ints_to_dict(self):
+        """Convert a string to the dict type."""
+
+        pass
+
+    def iterate_keys(self, x):
+        """Validate a list of integers via keys."""
+
+        return all(self.verify_key(i) for i in x)
+
+    def iterate_values(self, x):
+        """Validate a list of integers via values."""
+
+        return all(self.verify_value(i) for i in x)
+
     def validate(self):
         """Validate; raise an exception or return a verified dictionary."""
 
@@ -46,3 +82,12 @@ class ValidateIntegers:
 
             if self.verify_key(self.get_numerical_form(ints)):
                 return {ints: 1}
+
+        elif ints[0] in "([":
+            self.ints_to_list()
+
+        elif ints[0] == "{":
+            self.ints_to_dict()
+
+        else:
+            raise InvalidCharactersError("Int dictionary is invalid")
